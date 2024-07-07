36f8a6b50ad44f150a9b29c726c8265a8627e237
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-07-07 12:38:34 +0300

    'feat/react library'

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,27 @@ import {Title} from "ui/components/universal/Title";
 import style from 'styles/App.module.css'
 import s from 'styles/Title.module.css'
 import {Tasks} from "ui/components/Tasks";
-
+import {animated, useSpring} from '@react-spring/web'
 
 function App() {
+    const springs = useSpring({
+        from: {x: -1000},
+        to: {x: 100},
+    })
     return (
+        <animated.div
+            style={{
+                ...springs,
+            }}
+        >
+            <div className={style.appContainer}>
+                <div className={style.toDosContainer}>
+                    <Title title='Todos' className={s.title}/>
+                    <Tasks/>
+                </div>
 
-        <div className={style.appContainer}>
-            <div className={style.toDosContainer}>
-                <Title title='Todos' className={s.title}/>
-                <Tasks/>
             </div>
-
-        </div>
-
+        </animated.div>
     );
 }
 
